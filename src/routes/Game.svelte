986bd2f2e42c6e2d753ab04fb74881d836383cd2
katerynarieznik <rieznik.k@gmail.com>
2023-09-16 20:14:41 +0300

    add basic grid layout

---
+++ src/routes/Game.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Grid from './Grid.svelte';
+</script>
+
+<div class="game">
+	<div class="info" />
+
+	<div class="grid-container">
+		<Grid />
+	</div>
+
+	<div class="info" />
+</div>
+
+<style>
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		font-size: min(1vmin, 0.4rem);
+	}
+
+	.info {
+		width: 80em;
+		height: 10em;
+		background-color: plum;
+	}
+
+	.grid-container {
+		width: 80em;
+		height: 80em;
+		background-color: aqua;
+	}
+</style>
---
+++ src/routes/Square.svelte
@@ -0,0 +1,7 @@
+<div class="square" />
+
+<style>
+	.square {
+		background: aliceblue;
+	}
+</style>
