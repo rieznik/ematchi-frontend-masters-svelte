d9b1f894b8b928f818c1d3493ac4b5882bf60f60
katerynarieznik <rieznik.k@gmail.com>
2023-09-16 20:43:21 +0300

    add populate emojis logic

--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,7 +1,20 @@
-<div class="square" />
+<script lang="ts">
+	export let emoji: string;
+</script>
+
+<div class="square">
+	<span>{emoji}</span>
+</div>
 
 <style>
 	.square {
 		background: aliceblue;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	span {
+		font-size: 5em;
 	}
 </style>
