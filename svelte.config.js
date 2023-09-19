5f9a3cb6e93c900994411a8932779839fd4a2006
katerynarieznik <rieznik.k@gmail.com>
2023-09-19 15:17:18 +0300

    render index.html

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			strict: false
+			fallback: 'index.html'
 		}),
 		paths: {
 			base: dev ? '' : '/ematchi-frontend-masters-svelte'
