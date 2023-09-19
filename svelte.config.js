e1630854a38794f8bb6f07301b712b172a037b46
katerynarieznik <rieznik.k@gmail.com>
2023-09-19 14:47:41 +0300

    fix build 2

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			strict: false,
-			pages: 'build'
+			pages: '/build'
 		}),
 		paths: {
 			base: dev ? '' : 'https://rieznik.github.io/'
